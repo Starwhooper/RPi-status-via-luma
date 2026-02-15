7d783b71c6d5a3cab741854836b4d0c3c023e635
Starwhooper <thiemo@schuff.eu>
2026-02-15 19:55:27 +0100

    Fix error messages and improve logging setup

--- status.py
+++ status.py
@@ -27,7 +27,7 @@ try:
  import sys
  import time
 except:
- sys.exit("\033[91m {}\033[00m" .format('any needed package is not aviable. Please check README.md to check which components shopuld be installed via pip3".'))
+ sys.exit("\033[91m {}\033[00m" .format('any needed package is not aviable. Please check README.md to check which components should be installed".'))
 
 ##### import config.json
 try:
@@ -38,17 +38,15 @@ except:
  sys.exit("\033[91m {}\033[00m" .format('exit: The configuration file ' + os.path.split(os.path.abspath(__file__))[0] + '/config.json does not exist or has incorrect content. Please rename the file config.json.example to config.json and change the content as required '))
 
 ##### configure logging
-#try:
-# if cf['logging']['level'] == "debug": logging_level = logging.DEBUG
-# elif cf['logging']['level'] == "info": logging_level = logging.INFO
-# elif cf['logging']['level'] == "error": logging_level = logging.ERROR
-# elif cf['logging']['level'] == "critical": logging_level = logging.CRITICAL
-# else: logging_level = logging.WARNING
-#except:
-# logging_level = logging.WARNING
-level = str(cf.get('logging', {}).get('level', '')).strip().upper()
-logging_level = getattr(logging, level, logging.WARNING) if level else logging.WARNING
- 
+try:
+ if cf['logging']['level'] == "debug": logging_level = logging.DEBUG
+ elif cf['logging']['level'] == "info": logging_level = logging.INFO
+ elif cf['logging']['level'] == "error": logging_level = logging.ERROR
+ elif cf['logging']['level'] == "critical": logging_level = logging.CRITICAL
+ else: logging_level = logging.WARNING
+except:
+ logging_level = logging.WARNING
+
 try:
  logging_file = cf['logging']['file']
 except:
@@ -61,7 +59,7 @@ logging.basicConfig(
  format='%(asctime)s:%(levelname)s:%(message)s'
 )
 
-##### import module demo_opts
+##### import module demo_opts from luma.examples
 try:
  sys.path.append(cf['luma']['demo_opts.py']['folder'])
  from demo_opts import get_device
@@ -87,7 +85,7 @@ def render_component(componentname, cf, draw, device, y, font, rectangle_y, term
         module = importlib.import_module(f'components.{componentname}')
         return module.render(cf, draw, device, y, font, rectangle_y, term)
     except ModuleNotFoundError:
-        draw.text((0, y), f'{componentname} module missing', font=font, fill='RED')
+        draw.text((0, y), f'MISS {componentname}', font=font, fill='RED')
         y += cf['linefeed']
         logging.error('components.%s module not found', componentname)
     except Exception as e:
@@ -96,14 +94,13 @@ def render_component(componentname, cf, draw, device, y, font, rectangle_y, term
         logging.exception('Error while rendering %s component: %s', componentname, e)
     return y
     
-###### set defaults
-hostname = str(socket.gethostname())
-
+###### set default Font
 if cf['font']['ttf'] == True:
  font = ImageFont.truetype(cf['font']['ttffile'], cf['font']['ttfsize'])
 else:
  font = ImageFont.load_default()
-# font = ImageFont.load("arial.pil")
+
+##### max row height
 
 ##### do output
 def stats(device):
@@ -115,7 +112,6 @@ def stats(device):
   logging.critical('maybe restart?')
   lastmessage = datetime(1977, 1, 1)
 
-# alert=''
  with canvas(device, dither=True) as draw:
   if cf['design'] == 'terminal':
    term = terminal(device, font)
@@ -131,6 +127,7 @@ def stats(device):
   else: offset_y = 0
   y=1 + offset_y
 
+  #calculate max row height
   rectangle_y = draw.textbbox(xy=(0,0), text='AQjgp,;Ä', font=font)[3]
 
   #check all components
@@ -138,7 +135,7 @@ def stats(device):
    try:
     y = render_component(componentname, cf, draw, device, y, font, rectangle_y, term if 'term' in locals() else None)
    except:
-    draw.text((0,y), 'component issue', font = font, fill = 'RED')
+    draw.text((0,y), 'ERR ' + componentname, font = font, fill = 'RED')
     y += cf['linefeed']
    if len(alert) >= 1:
     if cf['pushover']['messages'] == 1 and datetime.now() >= (lastmessage + timedelta(hours=1)):
@@ -149,7 +146,7 @@ def stats(device):
           "user": cf['pushover']['userkey'],
           "html": 1,
           "priority": 1,
-          "message": hostname + ' ' + alert,
+          "message": str(socket.gethostname()) + ' ' + alert,
           }
       )
       lastmessage = datetime.now()
