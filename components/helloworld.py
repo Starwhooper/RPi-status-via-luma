d24279532e3167fdd0a5152a8ea5d56269c055fa
Starwhooper <thiemo@schuff.eu>
2026-02-19 19:20:51 +0100

    Refactor render function for clarity and efficiency

--- components/helloworld.py
+++ components/helloworld.py
@@ -1,17 +1,16 @@
 import logging
-import time
 
-def render(cf, draw, device, y, font, rectangle_y, term=None):
-    try:
-        if cf.get('design') == 'beauty':
-            draw.text((0, y), 'Hello World', font=font, fill='Yellow')
-            y += cf['linefeed']
-        elif cf.get('design') == 'terminal' and term is not None:
-            term.println('Hello World')
-            time.sleep(2)
-        logging.debug('Hello World')
-    except Exception:
-        logging.exception('Error rendering helloworld')
-        draw.text((0, y), 'hello err', font=font, fill='RED')
-        y += cf.get('linefeed', 8)
-    return y
+def render(cf, draw, device, y, font, rectangle_y=None, term=None):
+ try:
+  outputtext = 'Hello World'   
+  if cf.get('design') == 'beauty':
+   draw.text((0, y), outputtext, font=font, fill='Yellow')
+   y += cf['linefeed']
+  elif cf.get('design') == 'terminal' and term is not None:
+   term.println(outputtext)
+  logging.debug('Hello World')
+ except Exception:
+  logging.exception('Error rendering helloworld')
+  draw.text((0, y), 'hello err', font=font, fill='RED')
+  y += cf.get('linefeed', 8)
+ return y
